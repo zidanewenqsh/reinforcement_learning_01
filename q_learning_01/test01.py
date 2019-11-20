9a19e4ec275011116bca48bcac39cb91e9974ee9
zidanewenqsh <zidanewenqsh@163.com>
2019-11-20 18:35:34 +0800

    201911201833

--- q_learning_01/test01.py
+++ q_learning_01/test01.py
@@ -1,5 +1,5 @@
 import torch
-
+import random
 q = torch.zeros(6,6)
 r = -torch.ones_like(q)
 
@@ -11,3 +11,30 @@ r[[1,4,5],5]=100
 print(r)
 # a, b=1
 # print(a)
+
+indices = list(range(r.size(0)))
+indices_ = list(range(r.size(0)))
+y_index = random.sample(indices,1)[0]
+x_index = random.sample(indices,1)[0]
+print("a",y_index,x_index)
+# indices.pop(y_index)
+# print(indices)
+# print(r[indices])
+j = 0
+for i in range(10):
+    y_index = random.sample(indices, 1)[0]
+    x_index = random.sample(indices, 1)[0]
+    j=0
+    while True:
+        mask = r[y_index] != -1
+        mask1 = mask.nonzero()
+        x_index_1 = torch.randint(0,mask1.size(0),size=(1,))[0]
+        x_index = mask1[x_index_1][0].item()
+        print("indices",x_index,y_index)
+        y_index = x_index
+
+        if r[y_index,x_index] == 100:
+            print(j, r[y_index, x_index])
+            break
+
+        j+=1
